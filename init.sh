bed9ff5f9191b003ebead94d3bbdad7bfdc0639c
Malintha Amarasinghe <malintha.prasan@gmail.com>
2020-08-10 08:25:49 +0000

    added changes profile

--- init.sh
+++ init.sh
@@ -8,19 +8,29 @@ cd wso2-iass-setup
 #install java
 cd java
 cat jdk-8u* > jdk-linux-x64.tar.gz
-tar -xvf jdk-linux-x64.tar.gz
+tar -xf jdk-linux-x64.tar.gz
 JDK_FOLDER_NAME=$(find . -maxdepth 1  -type d -printf '%P\n' | tail -n 1)
 cd -
 
-sudo update-alternatives --install "/usr/bin/java" "java" "$USER_DIR/wso2-iass-setup/java/$JDK_FOLDER_NAME/bin/java" 1
+export JDK_HOME=$USER_DIR/wso2-iass-setup/java/$JDK_FOLDER_NAME
+echo JDK_HOME is taken as $JDK_HOME
+
+sudo update-alternatives --install "/usr/bin/java" "java" "$JDK_HOME/bin/java" 1
 sudo update-alternatives --config java
 
-cat /etc/profile conf/profile > profile-cat
+envsubst <  conf/profile >  conf/profile.env
+
+cat /etc/profile conf/profile.env > profile-cat
+
+echo "Final profile"
+cat profile-cat
+echo "============="
+
 sudo mv profile-cat /etc/profile
 
 # install maven
-sudo apt update
-sudo apt install maven
+# sudo apt update
+# sudo apt install maven
 
 # install unzip
 sudo apt install unzip
